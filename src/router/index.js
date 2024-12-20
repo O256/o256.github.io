c2417a43abe3a3ad80c142b07deb0b3ccaa491ab
oliver256 <oliver256@qq.com>
2024-12-20 11:21:04 +0800

    调整

--- src/router/index.js
+++ src/router/index.js
@@ -7,10 +7,7 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: HomeView,
-      meta: {
-        title: 'Home',
-      },
+      component: HomeView
     },
     {
       path: '/docs',
