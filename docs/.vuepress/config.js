88e00f40506de44da2bdccf024067fbb56f55833
oliver256 <5028994@qq.com>
2024-12-21 09:07:00 +0800

    修改文档

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -5,10 +5,34 @@ import { defineUserConfig } from 'vuepress'
 export default defineUserConfig({
     base: '/docs/',
     title: 'docs',
-    avatar: '/docs/avatar.png',
 
     bundler: viteBundler(),
-    theme: defaultTheme(),
+    theme: defaultTheme({
+        logo: '/avatar.png',
+        navbar: [
+            {
+                text: '首页',
+                link: '/',
+            },
+            {
+                text: 'Redis',
+                children: [
+                    {
+                        text: '基础概念',
+                        link: '/redis/basics/',
+                    },
+                    {
+                        text: '高级特性',
+                        link: '/redis/advanced/',
+                    },
+                    {
+                        text: '源码剖析',
+                        link: '/redis/source/',
+                    },
+                ],
+            },
+        ],
+    }),
 
     head: [
         ['link', { rel: 'icon', href: '/docs/avatar.png' }]
